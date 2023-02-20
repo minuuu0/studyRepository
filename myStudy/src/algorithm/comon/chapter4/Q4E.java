219fa7eceea4c204886989ecd64c7028885967b3
Tiel0043 <rlamw2000@naver.com>
2023-02-20 11:17:29 +0900

    230215 소인수분해 알고리즘

--- myStudy/src/algorithm/comon/chapter4/Q4E.java
+++ myStudy/src/algorithm/comon/chapter4/Q4E.java
@@ -31,14 +31,18 @@ class MathU{
     public static ArrayList<Long> factorize(long n) {
         ArrayList<Long> factors = new ArrayList<>();
 
-        long i = 2;
-        while(n > 1){
-            if(n % i == 0){
-                n /= i;
-                factors.add(i);
-            }else{
-                i++;
+        for(long div = 2; div * div <= n; div++){
+            while (n % div == 0){
+                // 소인수 목록에 div 추가
+                factors.add(div);
+
+                // N에서 소인수만큼 나눈다
+                n /= div;
             }
+        }
+        if(n > 1){
+            // 소인수를 찾지못하고 남아있는 n이라면 반드시 소수다 이를 소인수에 추가
+            factors.add(n);
         }
 
         return factors;
