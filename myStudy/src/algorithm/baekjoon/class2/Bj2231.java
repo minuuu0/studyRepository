416ffc3dcbd2c3dd2354f4c09fd253cea72ddb40
Tiel0043 <rlamw2000@naver.com>
2023-02-22 19:52:57 +0900

    230220 (일) 백준 클래스2

--- myStudy/src/algorithm/baekjoon/class2/Bj2231.java
+++ myStudy/src/algorithm/baekjoon/class2/Bj2231.java
@@ -3,20 +3,32 @@ package algorithm.baekjoon.class2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Bj2231 {
     static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    private static int divSum(int n) {
+        for(int i = 1; i <= n; i++){
+            int num = i;
+            int sum = 0;
+
+            // 1 ~ 216까지 완전 탐색
+            while (num != 0){
+                sum += num % 10;
+                num /= 10;
+            }
+            if(sum + i == n){
+                return i;
+            }
+        }
+        return 0;
+    }
+
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
-        int ans = 0;
 
-        while(n > 10){
-            int a = n % 10; // 자리수별로 쪼개고
-            n - a;
-        }
-
+        int ans = divSum(n);
         System.out.println(ans);
+
     }
 }
