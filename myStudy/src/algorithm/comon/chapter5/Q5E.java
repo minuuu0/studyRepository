f437991c5078fa7fe824d120660a6e3ad2b86226
Tiel0043 <rlamw2000@naver.com>
2023-03-05 16:43:47 +0900

    230301 (수) 폭탄 제거

--- myStudy/src/algorithm/comon/chapter5/Q5E.java
+++ myStudy/src/algorithm/comon/chapter5/Q5E.java
@@ -1,9 +1,45 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Q5E {
     // 폭탄 제거
     static final Scanner sc = new Scanner(System.in);
+    private static boolean isAllRemovable(int n, Bomb[] bombs) {
+
+        // 제거해도 상관없는 폭탄
+        Queue<Bomb> removableBombs = new LinkedList<>();
+
+        // 제거 완료된 폭탄
+        ArrayList<Bomb> removedList = new ArrayList<>();
+
+        for (Bomb b : bombs){ // 모든 폭탄에 대해
+            if(b.getChildCount() == 0){
+                removableBombs.add(b);
+            }
+        }
+
+        while (removableBombs.size() > 0){
+            Bomb b = removableBombs.poll(); // 제거 가능 폭탄을 꺼낸다
+            b.remove(); // 제거 가능한 폭탄을 제거 (Parent Bomb의 카운트를 1감소)
+            removedList.add(b); // 제거 완료 리스트에 추가
+
+            for(Bomb p : b.getParentBombs()){ // 제거 가능폭탄의 ParentBomb를 가져와
+                if(p.getChildCount() == 0){
+                    // 그 값이 0이면 제거 가능한 값이 된다
+                    removableBombs.add(p);
+                }
+            }
+        }
+        if(removedList.size() == n){ // 제거한 개수가 폭탄수와 같으면 true
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+
     private static void testCase(int caseIndex) {
         int n = sc.nextInt(); // 폭탄 수
         int m = sc.nextInt(); // 연쇄 관계 수
@@ -14,13 +50,19 @@ public class Q5E {
         }
 
         for(int i = 0; i < m; i++){
-            int u = sc.nextInt(); // u가 해제되면
-            int v = sc.nextInt(); // v가 폭발한다
+            int u = sc.nextInt() - 1; // u가 해제되면
+            int v = sc.nextInt() - 1; // v가 폭발한다
 
             Bomb b = bombs[v];
             b.addParentBombs(bombs[u]); // v에 u가 폭발을 유발할 수 있는 리스트임을 추가한다
         }
+        boolean removable = isAllRemovable(n, bombs);
 
+        if(removable) {
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
     }
 
     public static void main(String[] args) {
@@ -43,5 +85,22 @@ class Bomb{
     void addParentBombs(Bomb child){
         this.parentBombs.add(child);
         child.childCount++;
+    }
+
+    int getChildCount(){
+        return childCount;
+    }
+
+
+    public void remove() {
+        // 해당 폭탄을 제거한다
+        for(int i = 0; i < parentBombs.size(); i++){
+            Bomb p = parentBombs.get(i);
+            p.childCount -= 1;
+        }
+    }
+
+    public ArrayList<Bomb> getParentBombs() {
+        return parentBombs;
     }
 }
