6cf5aafa32d1919fed31dd7682a388afd000d5c7
Tiel0043 <rlamw2000@naver.com>
2023-03-27 10:23:51 +0900

    230327(월) 백준11653 -  단계별로 풀어보기 약수와 배수(1문제)

--- myStudy/src/algorithm/baekjoon/step/factor/Bj11653.java
+++ myStudy/src/algorithm/baekjoon/step/factor/Bj11653.java
@@ -1,2 +1,34 @@
-package algorithm.baekjoon.step.factor;public class Bj11653 {
+package algorithm.baekjoon.step.factor;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Bj11653 {
+    static final Scanner sc = new Scanner(System.in);
+    private static ArrayList<Integer> getPrimeFactor(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int i = 2;
+
+        while (n > 1){
+            if(n % i == 0){
+                list.add(i);
+                n /= i;
+                i = 2;
+            }
+            else {
+                i++;
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        ArrayList<Integer> list = getPrimeFactor(n);
+        for(int l : list){
+            System.out.println(l);
+        }
+
+    }
 }
