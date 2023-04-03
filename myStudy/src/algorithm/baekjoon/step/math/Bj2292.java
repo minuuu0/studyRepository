4dcea8ccb282f0ec575aa7e850ca2d433d1e97c2
Tiel0043 <rlamw2000@naver.com>
2023-04-03 21:31:44 +0900

    230403(월) 1문제 - 단계별로 풀어보기 일반수학1

--- myStudy/src/algorithm/baekjoon/step/math/Bj2292.java
+++ myStudy/src/algorithm/baekjoon/step/math/Bj2292.java
@@ -1,2 +1,23 @@
-package algorithm.baekjoon.step.math;public class Bj2292 {
+package algorithm.baekjoon.step.math;
+
+import java.util.Scanner;
+
+public class Bj2292 {
+    static final Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = sc.nextInt(); // n번방까지 갈 때 몇개의 방을 지나가는지
+        int room = 1; // 지나가는 방 회수 카운트
+        int range = 2;
+
+        if(n == 1) System.out.println(1);
+
+        else{
+            while (range <= n){
+                range = range + (6 * room);
+                room++;
+            }
+            System.out.println(room);
+        }
+
+    }
 }
