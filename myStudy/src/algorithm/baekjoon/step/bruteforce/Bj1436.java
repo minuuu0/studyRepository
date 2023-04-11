694889c4decf3e5b3a5b50d2580e6761c3d0d2ed
Tiel0043 <rlamw2000@naver.com>
2023-04-11 18:31:24 +0900

    2304011(화) 1문제 - 단계별로 풀어보기 브루트포스

---
+++ myStudy/src/algorithm/baekjoon/step/bruteforce/Bj1436.java
@@ -0,0 +1,99 @@
+package algorithm.baekjoon.step.bruteforce;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj1436 {
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static void func(int n) throws IOException{
+        int count = 1;
+        int prev_digit = 0; // 선수 자릿수
+        int num = 0; // 선수 자릿수를 제외한 나머지 뒷 자릿수
+
+        while (true){
+            // 선수 자릿수가 x..666x이면서 x..666이 아닌 경우
+            if((prev_digit % 10000) / 10 == 666 && prev_digit % 10 != 6){
+                for(int i = 0; i < 1000; i++){
+                    if(count == n){
+                        System.out.println(prev_digit * 1000 + num);
+                        return;
+                    }
+                    num++;
+                    count++;
+                }
+                prev_digit++;
+            }
+
+            // 선수 자릿수가 X...666 일 경우 (ex. 666_000, 1666_004, ...)
+            else if (prev_digit % 1000 == 666) {
+                num = 0;
+                for (int i = 0; i < 1000; i++) {
+
+                    if (count == n) {
+                        System.out.print(prev_digit * 1000 + num);
+                        return;
+                    }
+                    count++;
+                    num++;
+                }
+                prev_digit++;
+            }
+
+            // 선수 자릿수가 X...66 일 경우 (ex. 66_600, 166_600, ...)
+            else if (prev_digit % 100 == 66) {
+                num = 600;
+                for (int i = 0; i < 100; i++) {
+                    if (count == n) {
+                        System.out.print(prev_digit * 1000 + num);
+                        return;
+                    }
+                    count++;
+                    num++;
+                }
+                prev_digit++;
+
+            }
+
+
+            // 선수 자릿수가 X...6 일 경우 (ex. 6_660, 16_663, ...)
+            else if (prev_digit % 10 == 6) {
+                num = 660;
+                for (int i = 0; i < 10; i++) {
+                    if (count == n) {
+                        System.out.print(prev_digit * 1000 + num);
+                        return;
+                    }
+                    num++;
+                    count++;
+                }
+                prev_digit++;
+            }
+
+            // 그 외의 경우 (ex. 241_666, 23_666 ...)
+            else {
+                num = 666;
+                if (count == n) {
+                    System.out.print(prev_digit * 1000 + num);
+                    return;
+                }
+                count++;
+                prev_digit++;
+
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+
+        if(n > 1){
+            func(n);
+        }else{
+            System.out.println(666);
+        }
+
+    }
+}
