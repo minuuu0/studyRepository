f91dbc911c8f951ed2baefdacf8c2a1cee6fddf8
Tiel0043 <rlamw2000@naver.com>
2023-04-25 19:45:02 +0900

    230425(화) 백준11650  단계별로 풀어보기(정렬)

--- myStudy/src/algorithm/baekjoon/step/sort/Bj11650.java
+++ myStudy/src/algorithm/baekjoon/step/sort/Bj11650.java
@@ -1,2 +1,31 @@
-package algorithm.baekjoon.step.sort;public class Bj11650 {
+package algorithm.baekjoon.step.sort;
+
+import java.util.*;
+import java.io.*;
+public class Bj11650 {
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException{
+        int n = Integer.parseInt(br.readLine()); // 점의 개수
+
+        int[][] arr = new int[n][2];
+        for(int i = 0; i < n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+
+        }
+        Arrays.sort(arr, (e1, e2) -> { // 람다 함수로 우선순위 부여
+            if(e1[0] == e2[0]){
+                return e1[1] - e2[1];  // 음수가 나오면 우선순위가 낮다
+            }else{
+                return e1[0] - e2[0];  // 양수가 나오면 우선순위가 높다
+            }
+        });
+
+        for(int i = 0; i < n; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
 }
+
