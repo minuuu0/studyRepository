698fe13b2a3349dba221e36f95b3d6c995fc2142
Tiel0043 <rlamw2000@naver.com>
2023-04-29 18:04:09 +0900

    230428(금) 백준10814  단계별로 풀어보기(정렬)

--- myStudy/src/algorithm/baekjoon/step/sort/Bj10814.java
+++ myStudy/src/algorithm/baekjoon/step/sort/Bj10814.java
@@ -1,2 +1,42 @@
-package algorithm.baekjoon.step.sort;public class Bj10814 {
+package algorithm.baekjoon.step.sort;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Bj10814 {
+    static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        Person[] p = new Person[n];
+
+        for(int i = 0; i < n; i++){
+            p[i] = new Person(sc.nextInt(), sc.next());
+        }
+
+        Arrays.sort(p, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.age - o2.age;
+            }
+        });
+
+        for(Person peo : p){
+            System.out.println(peo.age + " " + peo.name);
+        }
+
+
+    }
+
+    static class Person{
+        int age;
+        String name;
+
+        Person(int age, String name){
+            this.age = age;
+            this.name = name;
+        }
+    }
+
 }
