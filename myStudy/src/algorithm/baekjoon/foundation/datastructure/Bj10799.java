4aa57070067b8b9433a7da218fdb53f5eff4e18f
Tiel0043 <rlamw2000@naver.com>
2023-05-14 15:18:28 +0900

    230512(금) 백준기초1 쇠막대기

--- myStudy/src/algorithm/baekjoon/foundation/datastructure/Bj10799.java
+++ myStudy/src/algorithm/baekjoon/foundation/datastructure/Bj10799.java
@@ -1,2 +1,32 @@
-package algorithm.baekjoon.foundation.datastructure;public class Bj10799 {
+package algorithm.baekjoon.foundation.datastructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Bj10799 {
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        String s = br.readLine();
+        Stack<Character> st = new Stack<>();
+
+        int result = 0;
+        for(int i = 0; i < s.length(); i++){
+            char charS = s.charAt(i);
+            if(charS == '('){
+                st.push('('); // 열린괄호면 push
+                continue;
+            }
+            if(charS == ')'){
+                st.pop(); // 일단 스택에서 pop 실행
+                if(s.charAt(i - 1) == '('){
+                    result += st.size(); // 현재 스택의 사이즈 만큼 더해줌
+                }else{ // 레이저가 아니면
+                    result++; // 1을 더한다
+                }
+            }
+        }
+        System.out.println(result);
+    }
 }
