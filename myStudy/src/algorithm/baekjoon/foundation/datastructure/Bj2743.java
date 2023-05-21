104711f4320b1e5af93474ad0a92c38b925d5df8
tiel0043 <rlamw2000@naver.com>
2023-05-21 14:30:20 +0900

    230521(일) 백준기초1 단어 길이 재기

--- myStudy/src/algorithm/baekjoon/foundation/datastructure/Bj2743.java
+++ myStudy/src/algorithm/baekjoon/foundation/datastructure/Bj2743.java
@@ -1,2 +1,15 @@
-package algorithm.baekjoon.foundation.datastructure;public class Bj2743 {
+package algorithm.baekjoon.foundation.datastructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj2743 {
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        String s = br.readLine();
+        System.out.println(s.length());
+    }
+
 }
