2a64c14d3decd41dd4bd8a0340d4b6938aaf83af
tiel0043 <rlamw2000@naver.com>
2023-06-22 08:54:17 +0900

    230620(화) 백준기초1 -진법 변환2 - 수학

--- myStudy/src/algorithm/baekjoon/foundation/math/Bj11005.java
+++ myStudy/src/algorithm/baekjoon/foundation/math/Bj11005.java
@@ -1,2 +1,33 @@
-package algorithm.baekjoon.foundation.math;public class Bj11005 {
+package algorithm.baekjoon.foundation.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bj11005 {
+
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken()); // n은 10억 이하
+        int b = Integer.parseInt(st.nextToken()); // b진법
+
+        while (n > 0){
+            int ch = n % b; // 나머지 연산
+            if(ch < 10){
+                // 정수라면
+                sb.append(ch);
+            }else{ // 문자라면
+                int sum = ch + 55;
+                sb.append((char)sum);
+            }
+            n /= b;
+        }
+
+        System.out.println(sb.reverse());
+    }
 }
