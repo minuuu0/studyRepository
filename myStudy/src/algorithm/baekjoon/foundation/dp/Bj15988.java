6c433d1b1f841d55ef1bb699219904a06f4564c0
tiel0043 <rlamw2000@naver.com>
2023-07-24 14:09:34 +0900

    230723(일) 백준기초1 _123 더하기3 - 다이나믹 프로그래밍

--- myStudy/src/algorithm/baekjoon/foundation/dp/Bj15988.java
+++ myStudy/src/algorithm/baekjoon/foundation/dp/Bj15988.java
@@ -1,4 +1,32 @@
 package algorithm.baekjoon.foundation.dp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Bj15988 {
+
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static long[] dp = new long[1000001];
+    static int mod = 1000000009;
+
+    public static void main(String[] args) throws IOException {
+        // dp 채워두기
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for(int i = 4 ; i < 1000001; i++){
+            dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % mod;
+        }
+
+        int t = Integer.parseInt(br.readLine()); // 테스트케이스 수
+
+        while (t-- > 0){
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(dp[n]);
+        }
+
+    }
+
 }
