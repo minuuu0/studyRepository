a4ab95afe039ab2bab24beda9a2f78bb68ab6db0
tiel0043 <rlamw2000@naver.com>
2023-07-28 11:45:18 +0900

    230726(수) 백준기초1 _오르막수- 다이나믹 프로그래밍

--- myStudy/src/algorithm/baekjoon/foundation/dp/Bj11057.java
+++ myStudy/src/algorithm/baekjoon/foundation/dp/Bj11057.java
@@ -1,4 +1,36 @@
 package algorithm.baekjoon.foundation.dp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Bj11057 {
+
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final int mod = 10007;
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int[][] dp = new int[n + 1][10];
+        int result = 0;
+
+        for(int i = 0; i < 10; i++){
+            dp[1][i] = 1; // 초기값
+        }
+
+        for(int i = 2; i < n + 1; i++){
+            for (int j = 0; j < 10; j++){ // dp[i][j] 각 자리수에 대해
+                for(int k = j; k < 10; k++){
+                    dp[i][j] = (dp[i - 1][k] + dp[i][j]) % mod;
+                }
+            }
+        }
+
+        for(int i = 0; i < 10; i++){
+            result = (result + dp[n][i]) % mod;
+        }
+        System.out.println(result % mod);
+
+    }
+
 }
