309498458d149e89b8b1f21b631a9da430ecfacf
tiel0043 <rlamw2000@naver.com>
2023-09-14 15:55:14 +0900

    230914(목) 백준기초1 _n 과 M(9)_ 브루트포스

--- myStudy/src/algorithm/baekjoon/foundation/bruteforce/Bj15663.java
+++ myStudy/src/algorithm/baekjoon/foundation/bruteforce/Bj15663.java
@@ -1,2 +1,62 @@
-package algorithm.baekjoon.foundation.bruteforce;public class Bj15663 {
+package algorithm.baekjoon.foundation.bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Bj15663 {
+
+    static int n, m;
+    static boolean[] checked;
+    static int[] area;
+    static int[] arr;
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    private static void solve(int k) {
+        if(k == m){
+            for (int i : area) {
+                sb.append(i + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int prev = -1; // 중복 제거를 위함(정렬시엔 중복이 연속되어 있다)
+        for(int i = 0; i < n; i++){
+            if (!checked[i] && prev != arr[i]) {
+                checked[i] = true;
+                area[k] = arr[i];
+                prev = arr[i];
+                solve(k + 1);
+                checked[i] = false;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        area = new int[m];
+        checked = new boolean[n];
+        arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+
+
+        int i = 0;
+        while (st.hasMoreTokens()) {
+            arr[i++] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        solve(0);
+
+        System.out.println(sb);
+    }
 }
