9327f29d208b025d01578bc8911b65f51b8ca910
tiel0043 <rlamw2000@naver.com>
2024-01-12 14:52:50 +0900

    240112(금) 백준기초2_2250번 트리 높이와 너비 복습_트리

---
+++ myStudy/src/algorithm/baekjoon/foundation/graph/Bj2250_2.java
@@ -0,0 +1,11 @@
+package algorithm.baekjoon.foundation.graph;
+
+import java.io.IOException;
+
+public class Bj2250_2 {
+
+    public static void main(String[] args) throws IOException {
+
+    }
+
+}
