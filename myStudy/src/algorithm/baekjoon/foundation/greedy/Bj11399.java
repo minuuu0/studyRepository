b8d9f81a7b3a1da70a6b64295d8a5d2f459871de
tiel0043 <rlamw2000@naver.com>
2024-01-22 21:08:27 +0900

    240118(목) 백준기초3_11399ATM_그리디

--- myStudy/src/algorithm/baekjoon/foundation/greedy/Bj11399.java
+++ myStudy/src/algorithm/baekjoon/foundation/greedy/Bj11399.java
@@ -1,2 +1,35 @@
-package algorithm.baekjoon.foundation.greedy;public class Bj11399 {
+package algorithm.baekjoon.foundation.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Bj11399 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int sum = 0;
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            sum = sum + arr[i];
+            res += sum;
+        }
+
+        System.out.println(res);
+    }
+
 }
