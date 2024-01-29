d066910378761d99bcd223c580326b16d34b6bbe
tiel0043 <rlamw2000@naver.com>
2024-01-29 13:02:51 +0900

    240129(월) 백준기초3_세 부분_브루트,구현

--- myStudy/src/algorithm/baekjoon/silver/Bj9322.java
+++ myStudy/src/algorithm/baekjoon/silver/Bj9322.java
@@ -1,2 +1,49 @@
-package algorithm.baekjoon.silver;public class Bj9322 {
+package algorithm.baekjoon.silver;
+
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+
+class Bj9322
+{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int[] nor;
+    static String[] sec;
+    static HashMap<String, Integer> map = new HashMap<>();
+
+    public static void main (String[] args) throws IOException
+    {
+
+        int t = Integer.parseInt(br.readLine());
+        while(t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            sec = new String[n];
+            nor = new int[n];
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                String key = st.nextToken();
+                map.put(key, i);
+            }
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                String key = st.nextToken();
+                nor[i] = map.get(key);
+            }
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                sec[nor[i]] = st.nextToken();
+            }
+
+            for (int i = 0; i < n; i++) {
+                System.out.print(sec[i] + " ");
+            }
+            System.out.println();
+
+
+        }
+    }
 }
