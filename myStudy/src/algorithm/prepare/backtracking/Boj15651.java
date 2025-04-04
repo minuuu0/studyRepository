66a6d24da02976598f3de2eec3267848015ba9f6
tiel0043 <rlamw2000@naver.com>
2025-04-04 19:10:06 +0900

    250404(금) n과m(3)(S3)

---
+++ myStudy/src/algorithm/prepare/backtracking/Boj15651.java
@@ -0,0 +1,38 @@
+package algorithm.prepare.backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj15651 {
+    static int N, M;
+    static int[] area;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer s = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(s.nextToken());
+        M = Integer.parseInt(s.nextToken());
+
+        area = new int[M];
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int depth) {
+        if (depth == M) {
+            for (int n : area) {
+                sb.append(n + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            area[depth] = i + 1;
+            dfs(depth + 1);
+        }
+    }
+}
