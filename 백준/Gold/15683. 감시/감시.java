5d0fb93465948298fcdec99724ef46cf36bc3aa4
MInwoo Kim <104603151+minuuu0@users.noreply.github.com>
2025-05-01 10:57:30 +0900

    [Gold III] Title: 감시, Time: 348 ms, Memory: 27032 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -1,61 +1,69 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
 
+class Main
+{
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-
-public class Main {
-    static int N, M;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int n, m;
     static int[][] board;
     static int[][] board2;
-    static ArrayList<CCTV> cctvList = new ArrayList<>();
+    static int mn = 0;
+    static List<Cctv> cctvList = new ArrayList<>();
 
-    // 남, 동, 북, 서
-    static int[] dx = {1, 0, -1, 0};
+    static int[] dx = {-1, 0, 1, 0}; // 하 상 우 좌
     static int[] dy = {0, 1, 0, -1};
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer s = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(s.nextToken());
-        M = Integer.parseInt(s.nextToken());
-        board = new int[N][M];
-        board2 = new int[N][M];
-        int mn = 0;
 
-        for (int i = 0; i < N; i++) {
-            s = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                board[i][j] = Integer.parseInt(s.nextToken());
-                if (board[i][j] != 0 && board[i][j] != 6) {
-                    cctvList.add(new CCTV(i, j));
-                }
-                if (board[i][j] == 0) mn++;
+    public static void main (String[] args) throws IOException
+    {
+        // 1. 모든 조합을 순회한다.
+        // 2. n, m을 순회하며 cctv를 만날 때마다 재귀 함수를 호출한다.
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        board = new int[n][m];
+        board2 = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                board2[i][j] = Integer.parseInt(st.nextToken());
+                if (board2[i][j] != 0 && board2[i][j] != 6) cctvList.add(new Cctv(i, j));
+                if (board2[i][j] == 0) mn++;
             }
         }
 
-        // 모든 경우의 수 뽑기
-        for (int d = 0; d < (1 << (2 * cctvList.size())); d++) {
-            // 마킹 초기화
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    board2[i][j] = board[i][j];
+        solve();
+    }
+
+    static void solve() {
+
+        int comb = (int) Math.pow(4, cctvList.size());
+        for (int c = 0; c < comb; c++) {
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    board[i][j] = board2[i][j];
                 }
             }
 
-            int brute = d;
-
-            // cctv 좌표 담고 upd 함수로 넘기기
+            // 모든 경우에 대해
+            int brute = c;
             for (int i = 0; i < cctvList.size(); i++) {
-                int dir = brute % 4;
-                brute /= 4;
                 int x = cctvList.get(i).x;
                 int y = cctvList.get(i).y;
+
+                // cctv의 방향 뽑기
+                int dir = brute % 4;
+                brute /= 4;
+
+                // 하 상 우 좌
                 if (board[x][y] == 1) {
                     upd(x, y, dir);
-                } else if (board[x][y] == 2) { // 반대 이동 (0,2) (1, 3)이 되도록
+                } else if (board[x][y] == 2) {
                     upd(x, y, dir);
                     upd(x, y, dir + 2);
                 } else if (board[x][y] == 3) {
@@ -72,12 +80,10 @@ public class Main {
                     upd(x, y, dir + 3);
                 }
             }
-
-
             int val = 0;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    if (board2[i][j] == 0) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (board[i][j] == 0) {
                         val++;
                     }
                 }
@@ -85,32 +91,32 @@ public class Main {
             mn = Math.min(mn, val);
         }
         System.out.println(mn);
-
     }
 
-    static void upd(int x, int y, int dir) {
+    private static void upd(int x, int y, int dir) {
         dir %= 4;
         while (true) {
             x += dx[dir];
             y += dy[dir];
-            if (OOB(x, y) || board2[x][y] == 6) {
+            if (!OOB(x, y) || board[x][y] == 6) {
                 return;
             }
-            if (board2[x][y] != 0) continue;
-            board2[x][y] = 7; // 7로 마킹
+            // cctv는 건너뛸 수 있다.
+            if (board[x][y] != 0) continue;
+            board[x][y] = 7;
+
         }
-
     }
 
-    // 범위를 나가면 참
-    static boolean OOB(int x, int y) {
-        return x < 0 || x >= N || y < 0 || y >= M;
+    private static boolean OOB(int x, int y) {
+        return x >= 0 && y >= 0 && x < n && y < m;
     }
-    static class CCTV {
+
+    static class Cctv {
         int x;
         int y;
 
-        CCTV(int x, int y) {
+        Cctv(int x, int y) {
             this.x = x;
             this.y = y;
         }
