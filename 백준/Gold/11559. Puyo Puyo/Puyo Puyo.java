9dcba8e0fb4693df6d54e2af3abb1a7e663814ee
MInwoo Kim <104603151+minuuu0@users.noreply.github.com>
2025-05-21 17:33:33 +0900

    [Gold IV] Title: Puyo Puyo, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Gold/11559. Puyo Puyo/Puyo Puyo.java
+++ 백준/Gold/11559. Puyo Puyo/Puyo Puyo.java
@@ -5,19 +5,17 @@ import java.util.*;
 
 public class Main {
 
-    static char[][] board;
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
-    static boolean[][] visited;
-    static ArrayList<Node> list;
     static int n = 12, m = 6;
+    static char[][] board = new char[n][m];
+    static int[] dx = {0, 1, -1, 0};
+    static int[] dy = {1, 0, 0, -1};
+    static boolean[][] visited;
+    static List<Node> list;
+    static int cnt = 0;
 
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        // 1. init
-        board = new char[n][m];
 
         for (int i = 0; i < n; i++) {
             String str = br.readLine();
@@ -26,34 +24,33 @@ public class Main {
             }
         }
 
-        int cnt = 0;
+        // 뿌요게임은 4개의 연속된 값이 없을 때까지 반복함
         while (true) {
-            boolean isFinished = true;
             visited = new boolean[n][m];
+            boolean isFinished = true;  // 만약 제거될 뿌요가 있다면 false로 전환
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < m; j++) {
                     if (board[i][j] != '.') {
                         list = new ArrayList<>();
-                        bfs(board[i][j], i, j);
-
+                        bfs(i, j, board[i][j]);
                         if (list.size() >= 4) {
-                            isFinished = false; // 연쇄가 일어났으므로 더 탐색해야함
+                            // 제거처리
+                            isFinished = false; // 계속해서 뿌요 처리
                             for (int k = 0; k < list.size(); k++) {
-                                board[list.get(k).x][list.get(k).y] = '.'; // 터트리기
+                                board[list.get(k).x][list.get(k).y] = '.';
                             }
                         }
                     }
                 }
             }
             if (isFinished) break;
-            fallPuyos();
+            fallDown();
             cnt++;
         }
         System.out.println(cnt);
-
     }
 
-    private static void fallPuyos() {
+    private static void fallDown() {
         for (int i = 0; i < m; i++) {
             for (int j = n - 1; j > 0; j--) {
                 if (board[j][i] == '.') {
@@ -69,9 +66,9 @@ public class Main {
         }
     }
 
-    private static void bfs(char c, int x, int y) {
-        Queue<Node> queue = new LinkedList<>();
+    private static void bfs(int x, int y, char target) {
         list.add(new Node(x, y));
+        Queue<Node> queue = new LinkedList<>();
         queue.offer(new Node(x, y));
         visited[x][y] = true;
 
@@ -80,11 +77,10 @@ public class Main {
             for (int d = 0; d < 4; d++) {
                 int nx = current.x + dx[d];
                 int ny = current.y + dy[d];
-
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && !visited[nx][ny] && board[nx][ny] == c) {
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m && !visited[nx][ny] && board[nx][ny] == target) {
+                    queue.offer(new Node(nx, ny));
                     visited[nx][ny] = true;
                     list.add(new Node(nx, ny));
-                    queue.offer(new Node(nx, ny));
                 }
             }
         }
@@ -94,11 +90,10 @@ public class Main {
         int x;
         int y;
 
-        public Node(int x, int y) {
+        Node(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
-
 
 }
